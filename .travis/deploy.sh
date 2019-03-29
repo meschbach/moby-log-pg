bdd1f473d3a60ade83bf99b4647215c23d7b8c04
Mark Eschbach <meschbach@gmail.com>
2019-03-29 15:24:55 -0700

    [ci/travis] Frist crack and a deployment pipeline

---
+++ .travis/deploy.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -ex
+
+# Figure out the version
+version=$(node -e 'process.stdout.write(require("./package.json").version)')
+image="meschbach/moby-log-pg:$version"
+
+# Build the Docker Image
+docker build . --tag "$image"
+
+# Build the plugin image
+id=$(docker create --name plugin-image "$image" true)
+docker export "$id" | tar -x -C moby-log-pg/rootfs
+cp config.json  moby-log-pg
+docker plugin create "$image" moby-log-pg
