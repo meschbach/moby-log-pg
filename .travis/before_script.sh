17f4c5ba3fe845c5cb7a355625216a291fb78f8a
Mark Eschbach <meschbach@gmail.com>
2019-03-28 21:12:44 -0700

    [ci/travis] Trying to just use Docker

--- .travis/before_script.sh
+++ .travis/before_script.sh
@@ -1,2 +1,13 @@
-psql -c "create database 'dev-docker-logs' identified by 'dev-docker-logs';" -U postgres
-psql -U dev-docker-logs <pg-01-base.sql
+#!/usr/bin/env bash
+# Based on https://github.com/doctrine/dbal/blob/master/tests/travis/install-postgres-11.sh
+
+set -ex
+
+echo "Preparing Postgres 11"
+
+sudo service postgresql stop || true
+
+sudo docker run -d --name postgres11 -p 5432:5432 postgres:11.1
+sudo docker exec -i postgres11 bash <<< 'until pg_isready -U postgres > /dev/null 2>&1 ; do sleep 1; done'
+
+echo "Postgres 11 ready"
