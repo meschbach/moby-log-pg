776dfc7b6d7fa88170f2b12473ce7162f730ccae
Mark Eschbach <meschbach@gmail.com>
2019-03-28 21:21:07 -0700

    [ci/travis] Pretty sure Postgres client isn't goign to be install

--- .travis/before_script.sh
+++ .travis/before_script.sh
@@ -1,13 +1,18 @@
 #!/usr/bin/env bash
+set -ex
+
+## Setup Postgres
 # Based on https://github.com/doctrine/dbal/blob/master/tests/travis/install-postgres-11.sh
 
-set -ex
 
 echo "Preparing Postgres 11"
 
 sudo service postgresql stop || true
-
 sudo docker run -d --name postgres11 -p 5432:5432 postgres:11.1
 sudo docker exec -i postgres11 bash <<< 'until pg_isready -U postgres > /dev/null 2>&1 ; do sleep 1; done'
 
 echo "Postgres 11 ready"
+
+# Add the user
+psql -c "CREATE ROLE 'dev-docker-logs' IDENTIFIED BY 'dev-docker-logs'" -U postgres
+PGPASSWORD=dev-docker-logs psql -U dev-docker-logs
