2188479f517cb6ae2b52e7e0b6d769d7749fc33d
Mark Eschbach <meschbach@gmail.com>
2019-03-29 16:24:56 -0700

    [cd]

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -5,8 +5,12 @@ set -ex
 version=$(node -e 'process.stdout.write(require("./package.json").version)')
 image="meschbach/moby-log-pg:$version"
 
+# Login
+docker login -u "$DOCKER_USER" -p "$DOCKER_PASS"
+
 # Build the Docker Image
 docker build . --tag "$image"
+docker push "$image"
 
 # Build the plugin image
 id=$(docker create --name plugin-image "$image" true)
@@ -14,3 +18,4 @@ mkdir -p $PWD/moby-log-pg/rootfs
 docker export "$id" | tar -x -C $PWD/moby-log-pg/rootfs
 cp config.json moby-log-pg
 docker plugin create "$image" moby-log-pg
+docker plugin push "$image"
