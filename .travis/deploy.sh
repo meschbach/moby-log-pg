87749e236a7f3737b8a18edc629c38d450219994
Mark Eschbach <meschbach@gmail.com>
2019-03-29 17:43:38 -0700

    [cd] Publish to meschbach/moby-log-pg-plugin

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -4,6 +4,7 @@ set -ex
 # Figure out the version
 version=$(node -e 'process.stdout.write(require("./package.json").version)')
 image="meschbach/moby-log-pg:$version"
+plugin="meschbach/moby-log-pg-plugin:$version"
 
 # Login
 docker login -u "$DOCKER_USER" -p "$DOCKER_PASS"
@@ -17,5 +18,5 @@ id=$(docker create --name plugin-image "$image" true)
 mkdir -p $PWD/moby-log-pg/rootfs
 docker export "$id" | tar -x -C $PWD/moby-log-pg/rootfs
 cp config.json moby-log-pg
-docker plugin create "$image" moby-log-pg
-docker plugin push "$image"
+docker plugin create "$plugin" moby-log-pg
+docker plugin push "$plugin"
